e89b3e28082b288e4e2a6413a3e7b05202ff4e1c
Tomás Silveira Salles <salles.twoa@gmail.com>
2021-04-03 17:11:01 +0200

    Finished first implementation of fizz-buzz.

--- src/fizz_buzz.clj
+++ src/fizz_buzz.clj
@@ -1,6 +1,12 @@
 (ns fizz-buzz)
 
-(defn fizz_buzz [] (map str (range 1 101)))
+(defn fizz_buzz []
+  (for [x (range 1 101)]
+    (condp #(zero? (rem %2 %1)) x
+      15 "FizzBuzz"
+      3 "Fizz"
+      5 "Buzz"
+      (str x))))
 
 (defn -main []
   (dorun (map println (fizz_buzz))))
--- test/fizz_buzz_test.clj
+++ test/fizz_buzz_test.clj
@@ -7,5 +7,27 @@
   (let [lazy_result (fizz_buzz)]
     (is (sequential? lazy_result))
     (let [vec_result (vec lazy_result)]
-      (is (= (count vec_result) 100))
-      (is (= (first vec_result) "1")))))
+      (is (= 100 (count vec_result)))
+      (dorun
+        (for [
+              n (range 15 101 15)
+              :let [i (dec n)]]
+          (is (= "FizzBuzz" (get vec_result i)) n)))
+      (dorun
+        (for [
+              n (range 3 101 3)
+              :let [i (dec n)]
+              :when (not (zero? (rem n 5)))]
+          (is (= "Fizz" (get vec_result i)) n)))
+      (dorun
+        (for [
+              n (range 5 101 5)
+              :let [i (dec n)]
+              :when (not (zero? (rem n 3)))]
+          (is (= "Buzz" (get vec_result i)) n)))
+      (dorun
+        (for [
+              n (range 1 101)
+              :let [i (dec n)]
+              :when (not (or (zero? (rem n 3)) (zero? (rem n 5))))]
+          (is (= (str n) (get vec_result i)) n))))))
