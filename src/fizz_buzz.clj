23ceee28ced338eafa78cf8a60c781e7fdf8acc1
Tomás Silveira Salles <salles.twoa@gmail.com>
2021-04-05 17:40:00 +0200

    Finished fizz-buzz.

--- src/fizz_buzz.clj
+++ src/fizz_buzz.clj
@@ -2,10 +2,10 @@
 
 (defn fizz_buzz []
   (for [x (range 1 101)]
-    (condp #(zero? (rem %2 %1)) x
-      15 "FizzBuzz"
-      3 "Fizz"
-      5 "Buzz"
+    (case [(zero? (rem x 3)) (zero? (rem x 5))]
+      [true true] "FizzBuzz"
+      [true false] "Fizz"
+      [false true] "Buzz"
       (str x))))
 
 (defn -main []
